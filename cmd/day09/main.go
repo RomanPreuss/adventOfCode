edcefba1b23647f952dd1314268d5e21e1c0a607
Roman Preuß <romanpre@gmail.com>
2020-12-09 18:20:08 +0100

    Minor refactor

--- cmd/day09/main.go
+++ cmd/day09/main.go
@@ -17,12 +17,9 @@ func main() {
 
 	numbers := strings.Split(strings.TrimSpace(string(input)), "\n")
 	preambleSize := 25
-	for i := 0; i < len(numbers)-preambleSize-1; i++ {
-		window := numbers[i : i+preambleSize]
-		target, _ := strconv.Atoi(numbers[i+preambleSize])
-
-		fmt.Println("window", i)
-		fmt.Println(window, target)
+	for i := preambleSize; i < len(numbers); i++ {
+		window := numbers[i-preambleSize : i]
+		target, _ := strconv.Atoi(numbers[i])
 
 		contains := false
 		for x := 0; x < len(window)-1; x++ {
@@ -38,9 +35,8 @@ func main() {
 		}
 
 		if !contains {
-			fmt.Println("not in window:", target)
+			fmt.Println("Task1: not in window:", target)
 
-			contains = false
 			for x := 0; x < len(numbers)-1; x++ {
 				for y := x + 1; y < len(numbers); y++ {
 					window = numbers[x:y]
@@ -58,9 +54,8 @@ func main() {
 						sum += num
 					}
 					if sum == target {
-						contains = true
 						result := smallest + largest
-						fmt.Println("Found range", smallest, largest, result)
+						fmt.Println("Task2: Found range", smallest, largest, "result:", result)
 						return
 					}
 				}

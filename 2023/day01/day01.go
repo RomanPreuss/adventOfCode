959f0dc296ca7d620c1147b0b803f1080148cf81
Roman Preuß <romanpre@gmail.com>
2023-12-03 16:30:47 +0100

    Refactor day01

--- 2023/day01/day01.go
+++ 2023/day01/day01.go
@@ -2,10 +2,8 @@ package day01
 
 import (
 	"bufio"
-	"fmt"
 	"io"
 	"log"
-	"regexp"
 	"strconv"
 	"strings"
 	"unicode"
@@ -45,159 +43,6 @@ func ExtractNumbers(input io.Reader) []int {
 
 	}
 
-	return result
-}
-
-func ExtractNumbersV2(input io.Reader) []int {
-	scanner := bufio.NewScanner(input)
-	result := []int{}
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		lineNumbers := make([]rune, 0, 2)
-
-		numbers := FindNum(line)
-
-		if len(numbers) == 0 {
-			log.Fatal("no number found in ", line)
-		}
-
-		for _, r := range numbers {
-			if len(lineNumbers) == 0 {
-				lineNumbers = append(lineNumbers, r)
-			} else if len(lineNumbers) == 1 {
-				lineNumbers = append(lineNumbers, r)
-			} else {
-				lineNumbers[1] = r
-			}
-		}
-
-		if len(lineNumbers) != 2 {
-			lineNumbers = append(lineNumbers, lineNumbers[0])
-		}
-		number, err := strconv.Atoi(string(lineNumbers))
-		if err != nil {
-			log.Fatal(err)
-		}
-		result = append(result, number)
-	}
-
-	return result
-}
-
-func FindNum(input string) []rune {
-	result := []rune{}
-	// ignore last char to catch edge-cases like 'eighthree' where the 't' is shared by
-	// eighT and Three
-	pattern := regexp.MustCompile("([0-9])*(on)*(tw)*(thre)*(fou)*(fiv)*(si)*(seve)*(eigh)*(nin)*(zer)*")
-	matches := pattern.FindAllSubmatchIndex([]byte(input), len(input))
-
-	for _, m := range matches {
-
-		for i := 2; i < len(m); i++ {
-			if m[i] == -1 || m[i+1] == -1 {
-				continue
-			}
-			nextCharIndex := m[i+1]
-			if m[i+1]-m[i] == 1 {
-				// it's a digit
-				digit := input[m[i]:m[i+1]]
-				fmt.Println("digit", digit)
-				result = append(result, []rune(digit)[0])
-				continue
-			}
-			if nextCharIndex > len(input)-1 {
-				// end of line so no match
-				continue
-			}
-
-			numberCandidate := input[m[i]:m[i+1]]
-
-			switch numberCandidate {
-			case "on":
-				if input[nextCharIndex] == 'e' {
-					result = append(result, '1')
-				}
-			case "tw":
-				if input[nextCharIndex] == 'o' {
-					result = append(result, '2')
-				}
-			case "thre":
-				if input[nextCharIndex] == 'e' {
-					result = append(result, '3')
-				}
-			case "fou":
-				if input[nextCharIndex] == 'r' {
-					result = append(result, '4')
-				}
-			case "fiv":
-				if input[nextCharIndex] == 'e' {
-					result = append(result, '5')
-				}
-			case "si":
-				if input[nextCharIndex] == 'x' {
-					result = append(result, '6')
-				}
-			case "seve":
-				if input[nextCharIndex] == 'n' {
-					result = append(result, '7')
-				}
-			case "eigh":
-				if input[nextCharIndex] == 't' {
-					result = append(result, '8')
-				}
-			case "nin":
-				if input[nextCharIndex] == 'e' {
-					result = append(result, '9')
-				}
-			case "zer":
-				if input[nextCharIndex] == '0' {
-					result = append(result, '0')
-				}
-			}
-		}
-
-	}
-	return result
-}
-
-func toNumber(input string) int {
-	val, err := strconv.Atoi(input)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return val
-}
-
-func ConvertToNumber(input []string) []rune {
-	result := make([]rune, 0, len(input))
-	for _, num := range input {
-
-		switch num {
-		case "one":
-			result = append(result, '1')
-		case "two":
-			result = append(result, '2')
-		case "three":
-			result = append(result, '3')
-		case "four":
-			result = append(result, '4')
-		case "five":
-			result = append(result, '5')
-		case "six":
-			result = append(result, '6')
-		case "seven":
-			result = append(result, '7')
-		case "eight":
-			result = append(result, '8')
-		case "nine":
-			result = append(result, '9')
-		case "zero":
-			result = append(result, '0')
-		default:
-			result = append(result, []rune(num)[0])
-		}
-	}
 	return result
 }
 
@@ -252,14 +97,12 @@ func ParseNumbers(input string) []rune {
 		if len(window) < 3 {
 			continue
 		}
-		fmt.Println("window", string(window))
 
 		number, ok := getNumber(window)
 		if ok {
 			result = append(result, number)
 			// reset window and keep last rune to handle edge cases
 			window = []rune{window[len(window)-1]}
-			fmt.Println("reset window", string(window))
 		}
 
 	}
--- 2023/day01/day01_test.go
+++ 2023/day01/day01_test.go
@@ -44,55 +44,6 @@ func TestMain(t *testing.T) {
 	})
 }
 
-func TestRealNumbers(t *testing.T) {
-	input := `1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet`
-
-	t.Run("extract numbers", func(t *testing.T) {
-		result := day01.ExtractNumbersV2(strings.NewReader(input))
-
-		assert.Equal(t, []int{12, 38, 15, 77}, result)
-	})
-}
-
-func TestWrittenNumbers(t *testing.T) {
-	input := `two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen
-seven
-kjrqmzv9mmtxhgvsevenhvq7
-eighthree`
-
-	result := day01.ExtractNumbersV2(strings.NewReader(input))
-
-	assert.Equal(t, []int{29, 83, 13, 24, 42, 14, 76, 77, 97, 83}, result)
-}
-
-func TestFindNumberEdgeCases(t *testing.T) {
-	t.Run("multiple matches", func(t *testing.T) {
-		res := day01.FindNum("onetwoone")
-		assert.Equal(t, []rune{'1', '2', '1'}, res)
-	})
-	t.Run("merged numbers", func(t *testing.T) {
-		res := day01.FindNum("eighthree")
-		assert.Equal(t, []rune{'8', '3'}, res)
-	})
-	t.Run("written numbers and digits", func(t *testing.T) {
-		res := day01.FindNum("one2eighthree")
-		assert.Equal(t, []rune{'1', '2', '8', '3'}, res)
-	})
-	t.Run("written numbers and digits 2", func(t *testing.T) {
-		res := day01.FindNum("kjrqmzv9mmtxhgvsevenhvq7")
-		assert.Equal(t, []rune{'9', '7', '7'}, res)
-	})
-}
-
 func TestParseNumbers(t *testing.T) {
 	t.Run("331s2twonep", func(t *testing.T) {
 		res := day01.ParseNumbers("331s2twonep")
